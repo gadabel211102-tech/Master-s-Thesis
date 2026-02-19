d9f187326f6e726edc53bdc202c093a0b15d5d2b
gadabel211102-tech <gadabel211102@gmail.com>
2026-02-19 13:52:23 +0100

    Update author and date information in comments

--- 07c_cosmic_integration.py
+++ 07c_cosmic_integration.py
@@ -5,7 +5,6 @@ Script 07c: COSMIC Integration - Cancer Mutation Database Comparison
 Compares detected variants against COSMIC cancer mutation database.
 Identifies known cancer hotspots and novel cancer-associated variants.
 
-Author: Enhanced Genomics Analysis Pipeline
 Date: 2026-02-12
 
 NOTE ON STUDY DESIGN:
