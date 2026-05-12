519fea224c1b491f7c7ce095d38d4acc3c7edc31
gadabel211102-tech <gadabel211102@gmail.com>
2026-05-12 10:56:14 +0200

    Delete pipeline_config.local.sh

--- pipeline_config.local.sh
+++
@@ -1,6 +0,0 @@
-# Local machine-specific overrides for thesis pipeline launchers.
-# This file is ignored by git so your laptop paths stay private.
-
-export TFM_DOCS_DIR="/mnt/c/Users/gadab/OneDrive - Uppsala universitet/Documents/TFM/Docs"
-export PANEL_1000G="/home/gadeaalonsoj/1000g_grch38/integrated_call_samples_v3.20130502.ALL.panel"
-export COLLAB_CORE_XLSX="/mnt/c/Users/gadab/OneDrive - Uppsala universitet/Documents/TFM/Docs/clinical variables-snps/SNPS PROYECTO MAMA_ENDOMETRIO(8).xlsx"
